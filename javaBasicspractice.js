68508d4006607c1743130e974e6be84b63a9eb02
MuhammadAhmad471 <ahmadch6313@gmail.com>
2023-09-04 21:59:01 +0500

    Myjavaproject

---
+++ javaBasicspractice.js
@@ -0,0 +1,46 @@
+//Arithmatic operatos
+let a = "ahmad";
+let b = 10;
+console.log(a + b)
+console.log(typeof (a+b))
+const O = {
+    name: "ahmad",
+    class: "10th",
+    section: "A",
+}
+console.log(O);
+O .name = 'Aslam';
+console.log(O);
+O .color= 'green';
+console.log(O)
+
+const dict={
+    krapyuhluhs:"Meaning and example",
+
+    krauhs:"Meaning  example",
+
+    krapyugshluhs:"Meaning and examp"
+}
+console.log(dict)
+//Comparison operators
+
+ass = 10;
+ass +=20;
+console.log(ass)
+test1 = 10;
+test2 = "9";
+console.log("test1 ==  test2 is",test1 == test2)
+console.log("test1>test2 is", test1>test2)
+console.log("test===test2",test1===test2)
+console.log("test!==test2",test1!==test2)
+console.log("test!==test2",test1!==test2)
+
+//Logical operators
+
+let x =10;
+let y =12;
+console.log(x>y && x==10)
+console.log(x>y || y===12)
+console.log(!true)
+console.log(typeof y);
+
